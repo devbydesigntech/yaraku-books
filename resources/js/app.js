3d018734c987da6b87930737d9ed88bcf6ecc082
Chris <chrisliu0416@gmail.com>
2020-04-28 17:49:37 -0700

    add sweetalert2

--- resources/js/app.js
+++ resources/js/app.js
@@ -8,6 +8,53 @@ require('./bootstrap');
 
 window.Vue = require('vue');
 
+//Import Vue Filter
+require('./filter'); 
+
+//Import progressbar
+require('./progressbar'); 
+
+//Setup custom events 
+require('./customEvents'); 
+
+//Import View Router
+import VueRouter from 'vue-router'
+Vue.use(VueRouter)
+
+//Import Sweetalert2
+import Swal from 'sweetalert2'
+window.Swal = Swal
+const Toast = Swal.mixin({
+  toast: true,
+  position: 'top-end',
+  showConfirmButton: false,
+  timer: 3000,
+  timerProgressBar: true,
+  onOpen: (toast) => {
+    toast.addEventListener('mouseenter', Swal.stopTimer)
+    toast.addEventListener('mouseleave', Swal.resumeTimer)
+  }
+})
+window.Toast = Toast
+
+//Import v-from
+import { Form, HasError, AlertError } from 'vform'
+window.Form = Form;
+Vue.component(HasError.name, HasError)
+Vue.component(AlertError.name, AlertError)
+
+//Routes
+import { routes } from './routes';
+
+//Register Routes
+const router = new VueRouter({
+    routes, 
+    mode: 'hash',
+
+})
+
+
+
 /**
  * The following block of code may be used to automatically register your
  * Vue components. It will recursively scan this directory for the Vue
@@ -19,7 +66,7 @@ window.Vue = require('vue');
 // const files = require.context('./', true, /\.vue$/i)
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+// Vue.component('example-component', require('./components/ExampleComponent.vue').default);
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
@@ -29,4 +76,5 @@ Vue.component('example-component', require('./components/ExampleComponent.vue').
 
 const app = new Vue({
     el: '#app',
+    router
 });
