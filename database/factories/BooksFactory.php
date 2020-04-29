e3c7fadecd39ad947abd06852b28546f94b6070c
Chris <chrisliu0416@gmail.com>
2020-04-28 17:38:23 -0700

    create migration table and database seed

---
+++ database/factories/BooksFactory.php
@@ -0,0 +1,13 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Model;
+use Faker\Generator as Faker;
+
+$factory->define(App\Book::class, function (Faker $faker) {
+    return [
+        'title' => $faker->text(50),
+        'author' => $faker->name
+    ];
+});
